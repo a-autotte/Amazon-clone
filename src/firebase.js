a0c9af5f813ca97cd923f1f0dc1cd92ea3c311dc
unknown <alexis.autotte@gmail.com>
2021-03-07 14:49:22 -0800

    First commit 1- Header section beginning

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAvoCavwn7NcKVdVXpApZshlAKi1P7nbVg",
+    authDomain: "clone-42526.firebaseapp.com",
+    projectId: "clone-42526",
+    storageBucket: "clone-42526.appspot.com",
+    messagingSenderId: "14632938608",
+    appId: "1:14632938608:web:dd7209198f5fe6e04958b5",
+    measurementId: "G-DQ94W8XK5H"
+  };
