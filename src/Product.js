5be752dc0ba2e590b0f5132b068fb7373b0e69e6
unknown <alexis.autotte@gmail.com>
2021-03-14 08:25:11 -0700

    Sixth commit 1- CheckoutProduct page (completed)

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./Product.css";
 import { useStateValue } from './StateProvider';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function Product({ id, title, image, price, rating}) {
 
--- src/reducer.js
+++ src/reducer.js
@@ -17,8 +17,20 @@ export const getBasketTotal = (basket) =>
             };
 
         case "REMOVE_FROM_BASKET":
-            return {...state, basket: state.basket.filter(item => item.id !== action.id)}
-            break;
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
+
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Cant remove product (id: ${action.id} as its not in basket!`
+                )
+            }
+
+            return {...state, basket: newBasket}
+
         default:
             return state;
             
